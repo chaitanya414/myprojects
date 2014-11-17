729848bfd622156bf488b67c26f13779ccbb7cce
chaitu414 <chaitu.krishna414@gmail.com>
2014-11-18 00:29:02 +0530

    trail

---
+++ sample/src/sample/Trail.java
@@ -0,0 +1,12 @@
+package sample;
+
+public class Trail {
+
+	public static void main(String[] args) {
+		String abc="   org inal   ";
+		System.out.println(1+abc+1);
+		System.out.println(1+abc.replaceAll("\\s+", " ").trim()+1);
+		System.out.println(1+abc.trim()+1);
+	}
+
+}
